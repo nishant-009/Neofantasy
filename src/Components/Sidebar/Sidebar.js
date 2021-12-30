01b4fb0df348151c482d7e9a4cd869007124756d
Karan2310 <karandeep9619@gmail.com>
2021-12-30 20:15:01 +0530

    Sidebar Screen 4 added

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -10,6 +10,7 @@ import Pool from './Screens/Pools/Pool';
 import BuildTeam from './Screens/BuildTeam/BuildTeam';
 import Fantasy from './Screens/SelectFantasy/SelectFantasy';
 import Confirm from './Screens/ConfirmTeam/Confirm';
+import Success from '../../assests/Success.png'
 
 const steps = [
     {
@@ -73,28 +74,61 @@ const Sidebar = () => {
                 </div>
                 <div className="ps-3 pe-4">
                     <button onClick={handleNext} hidden={activeStep === maxSteps - 1} className='next'>Next</button>
-                    <button data-bs-toggle="modal" data-bs-target="#exampleModal" hidden={activeStep !== maxSteps - 1} className='next' >Save Team</button>
+                    <button data-bs-toggle="modal" href="#exampleModalToggle" hidden={activeStep !== maxSteps - 1} className='next' >Save Team</button>
                 </div>
             </div>
 
             {/* Modal */}
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog  modal-dialog-centered">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
+            <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content modal1">
                         <div class="modal-body">
-                            ...
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
+                            <div class="modal-body text-center">
+                                <h4 className='fw-600 mb-3'>Save Team Name</h4>
+                                <p>Now that you have created your fantasy 11
+                                    Name your team for better access next time</p>
+                                <input type="text" placeholder='Enter Team Name' className='w-100' />
+                                <div className="d-flex flex-column">
+                                    <button type="button" class="mt-4 mb-2" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal" data-bs-dismiss="modal">Save Team Name</button>
+                                    <button type="button" className="bg-light text-dark" data-bs-dismiss="modal" >No</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content modal2">
+                        <div class="modal-body">
+                            <div class="modal-body text-center">
+                                <img src={Success} alt="" />
+                                <h4 className='fw-600 mt-2' > @username’s <br /> Team 1
+                                    Was Created Successfully</h4>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Modal */}
+            {/* <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content modal1">
+                        <div class="modal-body text-center">
+                            <h4 className='fw-600 mb-3'>Save Team Name</h4>
+                            <p>Now that you have created your fantasy 11
+                                Name your team for better access next time</p>
+                            <input type="text" placeholder='Enter Team Name' className='w-100' />
+                            <div className="d-flex flex-column">
+                                <button type="button" class="mt-4 mb-2">Save Team Name</button>
+                                <button type="button" className="bg-light text-dark" data-bs-dismiss="modal">No</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> */}
         </>
     )
 }
